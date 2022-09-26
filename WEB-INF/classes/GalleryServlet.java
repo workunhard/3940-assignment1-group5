d2e6c4efb7b7a64af247f5f1d9575888aa681e4a
Shey Barpagga <sheybarpagga@gmail.com>
2022-09-25 22:36:36 -0700

    added delete functionality

--- WEB-INF/classes/GalleryServlet.java
+++ WEB-INF/classes/GalleryServlet.java
@@ -6,7 +6,8 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 public class GalleryServlet extends HttpServlet {
   private int mCount;
- 
+
+
   public void doGet(HttpServletRequest request,
       HttpServletResponse response)
       throws ServletException, IOException {
@@ -20,9 +21,6 @@ public class GalleryServlet extends HttpServlet {
         if(session.getAttribute("position") == null) {
             session.setAttribute("position", 0);
         }
-    // instead of hardcoding to the first image in the list as done below
-    // save search criteria as session variable and use it to filter the files in the above array
-    // check if prev or next button pressed and then rotate through the array accordingly
 
       ArrayList<String> photos = new ArrayList<>();
       try {
@@ -61,16 +59,16 @@ public class GalleryServlet extends HttpServlet {
     out.println("<body>");
     out.println("<p id=\"sessionId\"> Current User: " + session.getAttribute("USER_ID"));
     out.println("<div>");
-    out.println("<div>");
+    out.println("<div><form>");
     out.println("<img id = \"img_src\" src=./images/" + photos.get(position) + " alt=" + alt_text + " width=400 height=300>");
-    out.println("<div>");
+    out.println("<div></form>");
     out.println("<br>");
     out.println("<div class='button'>");
     out.println("<form action=\"/3940-assignment1-group5/gallerynextprev\" method=\"GET\">" +
             "<input type=\"submit\" value=\"Prev\" name=\"choice\">" +
             "<input type=\"submit\" value=\"Next\" name=\"choice\"></input>" +
             "</input></form>");
-    out.println("<form action=\"delete\" method=\"POST\"><input type=\"submit\" value=\"Delete\"></input></form>");
+    out.println("<form action=\"/3940-assignment1-group5/gallery\" method=\"POST\"><input type=\"submit\" value=\"Delete\"></input></form>");
     out.println("</div></div><br>");
     out.println("</form>");
     out.println("<div>");
@@ -81,6 +79,40 @@ public class GalleryServlet extends HttpServlet {
     out.println("</body></html>");
     out.close();
    }
+
+   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+      HttpSession session = request.getSession();
+       ArrayList<String> photos = new ArrayList<>();
+       try {
+           Class.forName("com.mysql.cj.jdbc.Driver");
+       } catch (ClassNotFoundException e) {
+           e.printStackTrace();
+       }
+       final String URL = "jdbc:mysql://localhost:3306/test";
+       final Properties connectionProperties = new Properties();
+       connectionProperties.put("user", "root");
+       connectionProperties.put("password", "Popcorn");
+       try{
+           Connection con = DriverManager.getConnection(URL, connectionProperties);
+           Statement searchDB = con.createStatement();
+           ResultSet rs = searchDB.executeQuery("SELECT filename, caption, datetaken FROM photos WHERE UserID = " + session.getAttribute("id") + "");
+           while(rs.next()) {
+               photos.add(rs.getString("filename"));
+           }
+           int position = (Integer) session.getAttribute("position");
+           if(position >= photos.size()) {
+               position = 0;
+           } else if(position < 0) {
+               position = photos.size() - 1;
+           }
+           Statement delete = con.createStatement();
+           delete.executeUpdate("DELETE FROM photos WHERE filename = '"+ photos.get(position) +"'");
+       } catch (SQLException e) {
+           e.printStackTrace();
+       }
+       response.sendRedirect("gallery");
+   }
+
    private boolean isLoggedIn(HttpServletRequest req) {
 		HttpSession session = req.getSession(false);
 		if (session == null || !req.isRequestedSessionIdValid()) {
